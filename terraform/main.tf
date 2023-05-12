c92c8921824f4e0226f89c08b58c53178ee0126f
Joshuel Nobre <nobrej1999@gmail.com>
2023-05-12 14:08:14 -0300

    adjsutment

--- terraform/main.tf
+++ terraform/main.tf
@@ -47,12 +47,24 @@ resource "azurerm_network_security_group" "default" {
   resource_group_name = var.rg
   security_rule {
     name                       = "SSH"
-    priority                   = "200"
+    priority                   = "1000"
     direction                  = "Inbound"
     access                     = "Allow"
     protocol                   = "Tcp"
     source_port_range          = "*"
     destination_port_range     = "22"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
+  security_rule {
+    name                       = "HTTP"
+    priority                   = "1002"
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "80"
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
